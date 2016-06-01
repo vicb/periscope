a193827646520fec5b7aa7c244eb0512455aa594
Misko Hevery <misko@hevery.com>
2016-06-01 13:48:01 -0700

    update to angular builds-continues

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
+import { bootstrap } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 import { PeriscopeAppComponent, environment } from './app/';
 
