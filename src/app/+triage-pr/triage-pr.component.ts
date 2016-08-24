d5b374ebf4a143ea0ac2a7c8fbe4bb7ae642b3b0
Victor Berchet <victor@suumit.com>
2016-08-24 11:59:32 -0700

    Material updates

--- src/app/+triage-pr/triage-pr.component.ts
+++ src/app/+triage-pr/triage-pr.component.ts
@@ -1,41 +1,11 @@
 import {Component} from '@angular/core';
-
 import {PrBoardService, Staleness} from '../pr-board.service';
-import {PrComponent} from '../pr/pr.component';
-import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
-import {MD_TOOLBAR_DIRECTIVES} from '@angular2-material/toolbar';
-import {MD_BUTTON_DIRECTIVES} from '@angular2-material/button';
-import {MD_CHECKBOX_DIRECTIVES} from '@angular2-material/checkbox';
-import {MD_RADIO_DIRECTIVES} from '@angular2-material/radio';
-import {MD_PROGRESS_CIRCLE_DIRECTIVES} from '@angular2-material/progress-circle';
-import {MD_PROGRESS_BAR_DIRECTIVES} from '@angular2-material/progress-bar';
-import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
-import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
-import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
-import {MD_ICON_DIRECTIVES, MdIconRegistry} from '@angular2-material/icon';
-import {MD_TABS_DIRECTIVES} from '@angular2-material/tabs';
-
 
 @Component({
   moduleId: module.id,
   selector: 'app-triage-pr',
   templateUrl: 'triage-pr.component.html',
-  styleUrls: ['triage-pr.component.css'],
-  directives: [
-    PrComponent, 
-    MD_SIDENAV_DIRECTIVES,
-    MD_CARD_DIRECTIVES,
-    MD_TOOLBAR_DIRECTIVES,
-    MD_BUTTON_DIRECTIVES,
-    MD_CHECKBOX_DIRECTIVES,
-    MD_RADIO_DIRECTIVES,
-    MD_PROGRESS_CIRCLE_DIRECTIVES,
-    MD_INPUT_DIRECTIVES,
-    MD_LIST_DIRECTIVES,
-    MD_PROGRESS_BAR_DIRECTIVES,
-    MD_ICON_DIRECTIVES,
-    MD_TABS_DIRECTIVES,
-  ],
+  styleUrls: ['triage-pr.component.css']
 })
 export class TriagePrComponent {
   constructor(public state: PrBoardService) {}
